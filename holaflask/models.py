4ee219cd62f22ba78203a633fed9ab80212d8398
JIGT78 <128942384+JIGT78@users.noreply.github.com>
2023-10-02 21:30:49 +0200

    feat: creamos la clase carta

---
+++ holaflask/models.py
@@ -0,0 +1,7 @@
+class Carta:
+    def __init__(self, palo, numero):
+        self.palo = palo
+        self.numero = numero
+
+    def __str__(self):
+        pass
--- holaflask/views.py
+++ holaflask/views.py
@@ -1,7 +1,10 @@
 from flask import render_template
 from . import app
+from .models import Carta
 
 
 @app.route("/")
 def home():
-    return render_template('base.html')
+    carta = Carta('oros', 2)
+    carta1 = Carta('copas', 5)
+    return render_template('base.html', carta=carta, otra_carta=carta1)
